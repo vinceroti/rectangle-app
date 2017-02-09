2e5f7ef84d46988a6a5b78889c7add7b447bc63b
Vince Roti <vince.roti@gmail.com>
2017-02-09 16:10:02 -0600

    add draggable package

--- src/components/App.js
+++ src/components/App.js
@@ -31,6 +31,7 @@ class App extends Component {
     this._addRect = this._addRect.bind(this);
   }
 
+
   _addRect() {
     let randColor = () => {
       let randNum = () => { return Math.floor(Math.random() * 256); } ;
