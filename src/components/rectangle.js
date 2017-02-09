3f1e2c5a7560395bd9d83a7063d554e66e540be7
Vince Roti <vince.roti@gmail.com>
2017-02-09 17:04:12 -0600

    inline styling for wrapper div

--- src/components/rectangle.js
+++ src/components/rectangle.js
@@ -3,19 +3,24 @@ import { ResizableBox } from 'react-resizable';
 import Draggable from 'react-draggable'; // Both at the same time
 
 const style = {
-  cursor: 'pointer',
-  padding: 10
+  wrapper: {
+    display: 'inline-block'
+  },
+  handle: {
+    cursor: 'pointer',
+    padding: 10
+  },
 };
 
 class Rectangle extends Component {
   render() {
 
     return (
-      <div>
+      <div style={style.wrapper} >
         <Draggable handle=".handle">
           <div>
            <ResizableBox color={this.props.customColor} width={200} height={120} lockAspectRatio={true}  minConstraints={[50, 30]}>
-            <div style={style} className="handle"></div>
+            <div style={style.handle} className="handle"></div>
           </ResizableBox>
           </div>
         </Draggable>
