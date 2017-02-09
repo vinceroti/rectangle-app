bed546738a1d823711afac36e9a88dcb247ae24b
Vince Roti <vince.roti@gmail.com>
2017-02-09 16:59:41 -0600

    add padding to handle and remove text

--- src/components/rectangle.js
+++ src/components/rectangle.js
@@ -3,13 +3,9 @@ import { ResizableBox } from 'react-resizable';
 import Draggable from 'react-draggable'; // Both at the same time
 
 const style = {
-  width: 200,
-  height: 120,
-  display: 'inline-block',
-  position: 'relative',
-  border: '1px solid black'
+  cursor: 'pointer',
+  padding: 10
 };
-
 
 class Rectangle extends Component {
   render() {
@@ -17,13 +13,12 @@ class Rectangle extends Component {
     return (
       <div>
         <Draggable handle=".handle">
-             <div>
-               <div className="handle">Drag from here</div>
-               <ResizableBox color={this.props.customColor} width={200} height={120} lockAspectRatio={true}  minConstraints={[50, 30]}/>
-             </div>
-           </Draggable>
-
-
+          <div>
+           <ResizableBox color={this.props.customColor} width={200} height={120} lockAspectRatio={true}  minConstraints={[50, 30]}>
+            <div style={style} className="handle"></div>
+          </ResizableBox>
+          </div>
+        </Draggable>
       </div>
     );
   }
