4cd8271f451dd6a35b0fc444d460e38abf5efb3c
Vince Roti <vince.roti@gmail.com>
2017-02-09 16:51:37 -0600

    it drags!

--- src/components/rectangle.js
+++ src/components/rectangle.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 import { ResizableBox } from 'react-resizable';
+import Draggable from 'react-draggable'; // Both at the same time
+
+const style = {
+  width: 200,
+  height: 120,
+  display: 'inline-block',
+  position: 'relative',
+  border: '1px solid black'
+};
 
 
 class Rectangle extends Component {
   render() {
+
     return (
-      <ResizableBox color={this.props.customColor} width={200} height={120} lockAspectRatio={true}  minConstraints={[50, 30]}/>
+      <div>
+        <Draggable handle=".handle">
+             <div>
+               <div className="handle">Drag from here</div>
+               <ResizableBox color={this.props.customColor} width={200} height={120} lockAspectRatio={true}  minConstraints={[50, 30]}/>
+             </div>
+           </Draggable>
+
+
+      </div>
     );
   }
 }
